99cbc739e7e007b600fc46a006aa7f25dcdc9e49
Henry Manning <hamman3131@gmail.com>
2024-06-22 16:43:57 -0400

    relative links and no self-linking

--- md-wiki/generate_html.py
+++ md-wiki/generate_html.py
@@ -6,8 +6,14 @@ import shutil
 def convert_md_to_html(md_content):
     return markdown2.markdown(md_content)
 
-def create_hyperlinks(md_content, files, output_dir):
-    links = {file_name.replace('_', ' '): f'{output_dir}/{file_name}.html' for file_name in [os.path.splitext(f)[0] for f in files]}
+def create_hyperlinks(md_content, files, output_dir, current_file):
+    # Generate relative links for each file
+    links = {file_name.replace('_', ' '): f'{file_name}.html' for file_name in [os.path.splitext(f)[0] for f in files]}
+    
+    # Remove current file from links
+    current_file_name = os.path.splitext(current_file)[0].replace('_', ' ')
+    if current_file_name in links:
+        del links[current_file_name]
     
     def replace_link(match):
         text = match.group(0)
@@ -24,18 +30,17 @@ def generate_home_page(directory, files, subdirectories, home_content, output_di
     table_of_contents = '<ul>'
     for subdir in subdirectories:
         description_file = os.path.join(directory, subdir, 'description.txt')
-        subdir_output = os.path.join(output_dir, subdir)
         if os.path.exists(description_file):
             with open(description_file, 'r') as f:
                 description = f.read().strip()
-            table_of_contents += f'<li><a href="{subdir_output}/home.html">{description}</a></li>'
+            table_of_contents += f'<li><a href="{subdir}/home.html">{description}</a></li>'
         else:
-            table_of_contents += f'<li><a href="{subdir_output}/home.html">{subdir}</a></li>'
+            table_of_contents += f'<li><a href="{subdir}/home.html">{subdir}</a></li>'
     
     for file in files:
         if file != 'home.md':
             file_name = os.path.splitext(file)[0]
-            table_of_contents += f'<li><a href="{output_dir}/{file_name}.html">{file_name.replace("_", " ")}</a></li>'
+            table_of_contents += f'<li><a href="{file_name}.html">{file_name.replace("_", " ")}</a></li>'
     table_of_contents += '</ul>'
     
     html_content = convert_md_to_html(home_content) + table_of_contents
@@ -52,17 +57,17 @@ def process_directory(directory, output_directory):
     if 'home.md' in files:
         with open(os.path.join(directory, 'home.md'), 'r') as f:
             home_content = f.read()
-        home_content = create_hyperlinks(home_content, files, output_directory)
+        home_content = create_hyperlinks(home_content, files, output_directory, 'home.md')
         generate_home_page(directory, files, subdirectories, home_content, output_directory)
     
     for file in files:
         if file != 'home.md':
             with open(os.path.join(directory, file), 'r') as f:
                 md_content = f.read()
-            md_content = create_hyperlinks(md_content, files, output_directory)
+            md_content = create_hyperlinks(md_content, files, output_directory, file)
             html_content = convert_md_to_html(md_content)
-            html_file = os.path.splitext(file)[0] + '.html'
-            with open(os.path.join(output_directory, html_file), 'w') as f:
+            html_file = os.path.join(output_directory, os.path.splitext(file)[0] + '.html')
+            with open(html_file, 'w') as f:
                 f.write(html_content)
     
     for subdir in subdirectories:
@@ -80,4 +85,3 @@ if __name__ == "__main__":
     output_directory = '/home/r0m/projects/md-wiki/html-output'  # Change this to your desired output directory
     clear_output_directory(output_directory)
     process_directory(root_directory, output_directory)
-    
